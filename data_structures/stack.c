ea6219154058bb32e4a47fd6f42046c84a00ffff
Prajakta Tipugade <122222331+prajaktatipugade@users.noreply.github.com>
2023-10-14 17:49:10 +0530

    Add files via upload

--- data_structures/stack.c
+++ data_structures/stack.c
@@ -1,160 +1,88 @@
-/**
- * Kyler Smith, 2017
- * Stack data structure implementation.
- */
 
-////////////////////////////////////////////////////////////////////////////////
-// INCLUDES
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#define N 50
+int stack[N];
+int top=-1;
 
-////////////////////////////////////////////////////////////////////////////////
-// MACROS: CONSTANTS
+void push();
+void pop();
+void display();
+void peek();
 
-////////////////////////////////////////////////////////////////////////////////
-// DATA STRUCTURES
-/**
- * creating a stucture with 'data'(type:int), two pointers 'next','pre' (type: struct node) .
- */
-struct node
-{
-    int data;
-    struct node *next;
-    struct node *pre;
-} * head, *tmp;
+int main(){
+	int ch;
 
-////////////////////////////////////////////////////////////////////////////////
-// GLOBAL VARIABLES
-int count = 0;
+	do{
+		printf("Enter choice: \n 1:push  2:pop 3.display 4.peek \t:");
+		scanf("%d",&ch);
+		
+		switch(ch){
+			
+			case 1:
+				push();
+			 	break;
+			case 2:
+				pop();
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				peek();
+				break;
+			default:
+				printf("Invalid input!!");
+		
+		}
+			
+			
+	}while(ch!=0);
+	
+	return 0;
+	
+}
+void push(){
+	int data;
+	if(top == N -1){
+		
+		printf("Stack is overflow");
+		
+	}
+	else{
+		printf("Enter Data:");
+		scanf("%d",&data);
+		top++;
+		stack[top]=data;
+	}
+}
+void pop(){
+	if(top  ==-1){
+		
+		printf("Stack underflow");
+		
+	}
+	else{
+		
+		int x;
+		x=stack[top];
+		printf("deleted element:%d",x);
+		top--;
+		
+	}
+	
+}
+void display(){
+	int i;	
+	for(i=top;i>=0;i--){
+		printf("%d\n",stack[i]);
+	}
 
-////////////////////////////////////////////////////////////////////////////////
-// FUNCTION PROTOTYPES
-void create();
-void push(int x);
-int pop();
-int peek();
-int size();
-int isEmpty();
-
-////////////////////////////////////////////////////////////////////////////////
-// MAIN ENTRY POINT
-
-int main(int argc, char const *argv[])
-{
-    int x, y, z;
-
-    create();
-    push(4);
-    x = pop();
-    // 4. Count: 0. Empty: 1.
-    printf("%d.\t\tCount: %d.\tEmpty: %d.\n", x, size(), isEmpty());
-
-    push(1);
-    push(2);
-    push(3);
-    x = pop();
-    y = pop();
-    // 3, 2. Count: 1. Empty: 0;
-    printf("%d, %d.\t\tCount: %d.\tEmpty: %d.\n", x, y, size(), isEmpty());
-    pop();  // Empty the stack.
-
-    push(5);
-    push(6);
-    x = peek();
-    push(7);
-    y = pop();
-    push(8);
-    z = pop();
-    // 1, 6, 7, 8. Count: 2. Empty: 0.
-    printf("%d, %d, %d.\tCount: %d.\tEmpty: %d.\n", x, y, z, size(), isEmpty());
-
-    return 0;
+}
+void peek(){
+	int x;
+	x=stack[top];
+	printf("Peek element:%d",x);
+	printf("\n");
 }
 
-/**
- * Initialize the stack to NULL.
- */
-void create() { head = NULL; }
-
-/**
- * Push data onto the stack.
- */
-void push(int x)
-{
-    if (head == NULL)
-    {
-        head = (struct node *)malloc(1 * sizeof(struct node));
-        head->next = NULL;
-        head->pre = NULL;
-        head->data = x;
-    }
-    else
-    {
-        tmp = (struct node *)malloc(1 * sizeof(struct node));
-        tmp->data = x;
-        tmp->next = NULL;
-        tmp->pre = head;
-        head->next = tmp;
-        head = tmp;
-    }
-    ++count;
-}
-
-/**
- * Pop data from the stack
- */
-int pop()
-{
-    int returnData;
-    if (head == NULL)
-    {
-        printf("ERROR: Pop from empty stack.\n");
-        exit(1);
-    }
-    else
-    {
-        returnData = head->data;
-
-        if (head->pre == NULL)
-        {
-            free(head);
-            head = NULL;
-        }
-        else
-        {
-            head = head->pre;
-            free(head->next);
-        }
-    }
-    --count;
-    return returnData;
-}
-
-/**
- * Returns the next value to be popped.
- */
-int peek()
-{
-    if (head != NULL)
-        return head->data;
-    else
-    {
-        printf("ERROR: Peeking from empty stack.");
-        exit(1);
-    }
-}
-
-/**
- * Returns the size of the stack.
- */
-int size() { return count; }
-
-/**
- * Returns 1 if stack is empty, returns 0 if not empty.
- */
-int isEmpty()
-{
-    if (count == 0)
-        return 1;
-    return 0;
-}
